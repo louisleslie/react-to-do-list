a6713fadeff59643e27ee7d139615240a3b99c5e
Louis Leslie <louis.leslie@gmail.com>
2021-08-16 08:56:46 +0100

    Added Privacy settings to fully remove all localstorage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
+import React from 'react';
 import {ToDoList} from './components/ToDoList';
 
 function App() {
   //localStorage.clear();
+  const [storageConsent, setStorageConsent] = React.useState((localStorage.getItem("storageConsent") === "true") ? true : (localStorage.getItem("storageConsent") === "false") ? false : "Not Set");
   const toDoData = JSON.parse(localStorage.getItem("todolistdata")) || {toDoListTasks:[], toDoListTime:0}
   const toDoTasks = toDoData.toDoListTasks
   const toDoTime = toDoData.toDoListTime
+
+  const setConsent = (e) => {
+    localStorage.setItem("storageConsent", e);
+    setStorageConsent(e);
+  }
+
+  const requestConsent = () => {
+    return (
+      <div>
+        <h2>This app can save your task list to your browser so you can access it later.</h2>
+        <p>Do you want to allow this?</p>
+        <button style={{color: "black"}} onClick={(e) => setConsent(true)}>Enable Task Storage</button><button style={{color: "black", marginLeft:"1rem"}} onClick={(e) => setConsent(false)}>Don't Enable Task Storage</button>
+      </div>
+    )
+  }
   //console.log(toDoData);
   //console.log(JSON.parse(localStorage.getItem("todolistdata")));
   return (
     <div className="App">
       <header className="App-header">
         <h1>Task Tracker</h1>
-        <p>Created by <a href="https://www.github.com/louisleslie" target="_blank">Louis Leslie</a></p>
+        <p>Created by <a href="https://www.github.com/louisleslie" target="_blank" rel="noreferrer">Louis Leslie</a></p>
       </header>
       <main>
-        <ToDoList toDoListTasks={toDoTasks} toDoListTime={toDoTime} />
+        {(Object.keys(localStorage).indexOf("storageConsent") === -1) ? requestConsent() : <ToDoList toDoListTasks={toDoTasks} toDoListTime={toDoTime} storageConsent={storageConsent} /> }
       </main>
     </div>
   );
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -5,11 +5,32 @@ import './ToDoList.css';
 const ToDoList = (props) => {
   const [toDoListTasks, setTasks] = React.useState(props.toDoListTasks); //Use an array of objects with the props for each, not the task instance itself!
   const [toDoListTime, setToDoListTime] = React.useState(props.toDoListTime);
+  const [storageConsent, setStorageConsent] = React.useState(props.storageConsent);
   const [formTaskTime, setFormTaskTime] = React.useState(0);
+  
   const saveTasks = (tasklist, totaltime) => {
-    localStorage.setItem("todolistdata", JSON.stringify({toDoListTasks: tasklist, toDoListTime: totaltime}));
+    if (storageConsent) {
+      localStorage.setItem("todolistdata", JSON.stringify({toDoListTasks: tasklist, toDoListTime: totaltime}));
+    }
   }
   
+  const removeConsent = (e) => {
+    localStorage.removeItem("todolistdata");
+    localStorage.setItem("storageConsent", false);
+    setStorageConsent(false);
+  }
+
+  const fullyRemoveConsent = (e) => {
+    localStorage.clear();
+    setStorageConsent(false);
+  }
+
+  const addConsent = (e) => {
+    setStorageConsent(true);
+    localStorage.setItem("storageConsent", true);
+    localStorage.setItem("todolistdata", JSON.stringify({toDoListTasks: toDoListTasks, toDoListTime: totalTime}));
+  }
+
   const removeTask = (removeTaskId) => {
     let currentTasks = toDoListTasks;
     let newTasks = [];
@@ -105,6 +126,27 @@ const ToDoList = (props) => {
     e.target.querySelector("#taskName").focus();
   }
 
+  const handlePrivacyClick = (e) => {
+    if (e.target === document.querySelector(".privacy-notice-wrapper")) {
+      //console.log("Click was on the Privacy Notice Wrapper!");
+      e.target.classList.add("hidden");
+      document.removeEventListener("click", handlePrivacyClick);
+    } else if (e.target === document.getElementById("privacy-notice-close")) {
+      //console.log("Click was on the Privacy Notice Close button!");
+      document.querySelector(".privacy-notice-wrapper").classList.add("hidden");
+      document.removeEventListener("click", handlePrivacyClick);
+    }
+  }
+
+  const showPrivacy = (event) => {
+    document.querySelector(".privacy-notice-wrapper").classList.toggle("hidden");
+    if (!document.querySelector(".privacy-notice-wrapper").classList.contains("hidden")) {
+      //console.log("The privacy div is not hidden");
+      document.addEventListener("click", handlePrivacyClick);
+    } else {
+      document.removeEventListener("click", handlePrivacyClick);
+    }
+  }
   const listTasks = toDoListTasks.sort(function(x, y) {
     return (x.taskCompleted === y.taskCompleted)? 0 : x.taskCompleted? 1 : -1;
   }).map(function(task) {
@@ -125,8 +167,21 @@ const ToDoList = (props) => {
         </div>
         <div className="to-do-list-summary">
           <p>Completed Tasks: {totalCompleted()} / {toDoListTasks.length} </p>
-          <p>Estimated Time to Complete:  {remainingTime(toDoListTasks)}</p>
+          <p>Estimated Time to Complete:  {toDoListTime}</p>
           <p>Total Time:  {totalTime()}</p>
+        </div>
+        <div className="to-do-list-storage">
+          {(storageConsent) ? <button onClick={(e) => removeConsent(true)}>Delete My Data</button> : <button onClick={(e) => addConsent(true)}>Save my Data</button>}
+          <br></br>
+          <button className="privacy-notice-link" onClick={(e) => showPrivacy()}>Learn more</button>
+          <div className="privacy-notice-wrapper hidden">
+            <div className="privacy-notice">
+              <button id="privacy-notice-close" className="privacy-notice-close" style={{color: 'black'}}>X</button>
+              <h3>Data Storage</h3>
+              <p>This site can store your tasks in your browser's local storage, enabling you to access them when you reload the page. Your data stays on your device, but as a browser's local storage data can be accessed very easily, we do not reccommend using the site to store sensitive or personal data. </p>
+              {(storageConsent) ? <div><p>You are currently allowing the site to save your data.</p><p>If you want to remove the saved data, you can just click the button below. This will remove any data the site holds in local storage, but your current tasks will remain on the page until you reload the page.</p><button onClick={(e) => fullyRemoveConsent(true)}>Delete My Data</button></div> : <div><p>Your data is not currently being saved. When you reload the page, any existing tasks will be lost.</p><button onClick={(e) => addConsent(true)}>Save my Data</button></div>}
+            </div>
+          </div>
         </div>
       </div>
       <div className="task-container">
